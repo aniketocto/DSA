cbb5868e2ad79218f718af7d974543e068c414ac
Aniket Khambal <125602678+aniketocto@users.noreply.github.com>
2026-07-02 21:48:55 +0530

    Time: 5 ms (79.78%) | Memory: 46 MB (53.28%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev = 0;
+        int og = x;
+        while(x > 0)
+        {
+            int digit = x % 10;
+            rev = rev * 10 + digit; 
+            x = x / 10;
+        } 
+
+        return og == rev;
+    }
+}
