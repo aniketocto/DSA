28b13b9cf6ee72d782a3f804068d1cb0e3abf2b0
Aniket Khambal <125602678+aniketocto@users.noreply.github.com>
2026-07-13 21:39:51 +0530

    Time: 8 ms (67.74%) | Memory: 46 MB (90.84%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -4,21 +4,25 @@ class Solution {
         int r = 0;
         int maxL = 0;
         int maxFreq = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
+        // HashMap<Character, Integer> map = new HashMap<>();
+        int[] arr = new int[26];
 
         while(r < s.length())
         {
-           map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
-           maxFreq = Math.max(maxFreq, map.get(s.charAt(r)));
+        //    map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
+        //    maxFreq = Math.max(maxFreq, map.get(s.charAt(r)));
+            arr[s.charAt(r) - 'A']++;
+            maxFreq = Math.max(maxFreq, arr[s.charAt(r) - 'A']);
 
            while ((r - l + 1) - maxFreq > k)
            {
-                map.put(s.charAt(l), map.getOrDefault(s.charAt(l), 0) - 1);
-                if(map.get(s.charAt(l)) == 0)
-                {
-                    map.remove(s.charAt(l));
-                }
-            l++;
+                // map.put(s.charAt(l), map.getOrDefault(s.charAt(l), 0) - 1);
+                arr[s.charAt(l) - 'A']--;
+                // if(map.get(s.charAt(l)) == 0)
+                // {
+                    // map.remove(s.charAt(l));
+                // }
+                l++;
            }
            maxL = Math.max(maxL, r-l+1);
             r++;
