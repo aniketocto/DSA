43b796c3fd3af6f8d8fa28a5ef2b26a964bd81d1
Aniket Khambal <125602678+aniketocto@users.noreply.github.com>
2026-07-13 22:21:33 +0530

    Time: 3 ms (81.46%) | Memory: 43.8 MB (14.23%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < operations.length; i++)
+        {
+            if(operations[i].equals("+"))
+            {
+                int top1 = stack.peek();
+                stack.pop();
+                int top2 = stack.peek();
+                stack.push(top1);
+                stack.push(top1+top2);
+            }
+            else if(operations[i].equals("D"))
+            {
+                int top = stack.peek();
+                stack.push(top*2);
+            }
+            else if(operations[i].equals("C"))
+            {
+                stack.pop();
+            }
+            else
+            {
+                stack.push(Integer.parseInt(operations[i]));
+            }
+        }
+        int sum = 0;
+        if(!stack.isEmpty())
+        {
+            for(int num: stack)
+                sum+=num;
+        }
+        return sum;
+    }
+}
